75f775b322fbe3cc51d6235d876a31c77e7d9703
Matías Insaurralde <matias@insaurral.de>
2025-07-12 22:49:49 -0300

    Fix tests
    Signed-off-by: Matías Insaurralde <matias@insaurral.de>

---
+++ src/App.test.tsx
@@ -0,0 +1,11 @@
+import { describe, it, expect } from "bun:test";
+
+describe("App", () => {
+  it("has basic structure", () => {
+    expect(true).toBe(true);
+  });
+
+  it("can run tests", () => {
+    expect(1 + 1).toBe(2);
+  });
+}); 
